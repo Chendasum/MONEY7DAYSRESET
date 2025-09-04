ae8bc76586480effa2b6429938710d319c7ca78f
Chendasum <chendasum168@gmail.com>
2025-09-04 20:28:40 +0700

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,13 @@
+\
+"use strict";
+
+module.exports = {
+  db: safeRequire("./db") || {},
+  storage: safeRequire("./storage") || {},
+  bot: safeRequire("./bot")?.bot || null,
+  initBotWebhook: safeRequire("./bot")?.initBotWebhook || (async () => {}),
+};
+
+function safeRequire(p) {
+  try { return require(p); } catch (e) { return null; }
+}
